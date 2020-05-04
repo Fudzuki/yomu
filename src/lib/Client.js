c5bf7d91b4e250da024064621fac4b1f0b1d1bbe
Fudzuki <44330924+Fudzuki@users.noreply.github.com>
2020-05-04 18:57:24 +0900

    Create Client.js

---
+++ src/lib/Client.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js');
+const { Permissions, Permissions: { FLAGS } } = Discord;
+const path = require('path');
+
+const CommandStore = require('./structures/CommandStore');
+const EventStore = require('./structures/EventStore');
