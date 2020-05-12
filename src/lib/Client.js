9a5a8b5ad65d7f4b3d2b3a5857b34d2c0aba2629
Fudzuki <44330924+Fudzuki@users.noreply.github.com>
2020-05-12 22:49:26 +0900

    Delete Client.js

--- src/lib/Client.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-const { Permissions, Permissions: { FLAGS } } = Discord;
-const path = require('path');
-
-const CommandStore = require('./structures/CommandStore');
-const EventStore = require('./structures/EventStore');
